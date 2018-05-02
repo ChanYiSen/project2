2e9191df1b782fa5b09d75dc9d6eaef64a773175
armst522 <35544317+armst522@users.noreply.github.com>
2018-05-02 16:18:33 -0500

    Add files via upload

---
+++ js/darlingredesign.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+// all jquery and some javascript commands go here
+
+	$(".title").delay(1000).fadeIn().delay(1000).fadeOut();
+	$(".container").delay(2500).fadeIn();
+	$("#hometitle").delay(2500).fadeIn();
+	$(".menu").delay(2500).fadeIn();
+
+
+});
